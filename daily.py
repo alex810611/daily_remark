c9a40edbf8528b2e3dc7a53cddc36b59412fac4d
alex810611 <alex810611@gmail.com>
2022-10-09 20:00:22 +0800

    加入讀取

--- daily.py
+++ daily.py
@@ -1,3 +1,16 @@
+#讀取檔案
+products =[]
+with open('products.csv','r') as f :
+	for line in f :
+		if '商品,價格' in line:
+			continue 
+		name,price,date = line.strip().split(',') #strip去除\n 再用split ,切割
+		products.append([name,price,date])
+
+print(products)
+
+#記帳程式，可存成CSV
+
 products = []
 
 while True :
@@ -6,18 +19,19 @@ while True :
 		break
 	price = input('請輸入商品價格')
 	price = int(price)
+	date = input('xxxx年,xx月,xx日')
 
 	#p =[]
 	#p.append(name)
 	#p.append(price)
 	#p = [name , price]
-	products.append([name,price])
+	products.append([name,price,date])
 
 for p in products: #列出清單中所有資訊
-	print(p[0],'的價格是',p[1])
+	print(p[0],'的價格是',p[1],'日期',p[2])
 
 
 with open ('products.csv' , 'w', encoding = 'utf-8') as f :      #打開檔案
-	f.write('商品,價格\n')
+	f.write('商品,價格,日期\n')
 	for p in products:                       #for 迴圈
-		f.write(p[0] + ',' + str(p[1]) + '\n' )    
+		f.write(p[0] + ',' + str(p[1]) + ',' + p[2]+'\n' )    

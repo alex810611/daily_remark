e247985c23baf36e7cecc86d939afe999c63de8e
alex810611 <alex810611@gmail.com>
2022-10-09 19:33:49 +0800

    存擋CSV

--- daily.py
+++ daily.py
@@ -5,6 +5,8 @@ while True :
 	if name == 'q' : #q 離開
 		break
 	price = input('請輸入商品價格')
+	price = int(price)
+
 	#p =[]
 	#p.append(name)
 	#p.append(price)
@@ -13,9 +15,9 @@ while True :
 
 for p in products: #列出清單中所有資訊
 	print(p[0],'的價格是',p[1])
-	
-
-	
 
 
-print(products)
+with open ('products.csv' , 'w', encoding = 'utf-8') as f :      #打開檔案
+	f.write('商品,價格\n')
+	for p in products:                       #for 迴圈
+		f.write(p[0] + ',' + str(p[1]) + '\n' )    

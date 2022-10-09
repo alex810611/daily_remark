e8c782b012377902e05bbac3d7bc1a96d58613ac
alex810611 <alex810611@gmail.com>
2022-10-09 23:43:36 +0800

    找尋檔案是否存在

--- daily.py
+++ daily.py
@@ -1,18 +1,19 @@
-#讀取檔案
+#檢察檔案是否存在
+import os
 products =[]
-with open('products.csv','r') as f :
-	for line in f :
-		if '商品,價格' in line:
-			continue 
-		name,price,date = line.strip().split(',') #strip去除\n 再用split ,切割
-		products.append([name,price,date])
+if os.path.isfile('products.csv'):
+	print('yes,找到檔案')
+	with open('products.csv','r') as f: #開啟檔案
+		for line in f :
+			if '商品,價格' in line:
+				continue 
+			name,price,date = line.strip().split(',') #strip去除\n 再用split ,切割
+			products.append([name,price,date])
+	print(products)
+else:
+	print('找不到檔案')
 
-print(products)
-
-#記帳程式，可存成CSV
-
-products = []
-
+#讓使用者輸入
 while True :
 	name = input('請輸入商品名稱')
 	if name == 'q' : #q 離開
@@ -20,17 +21,15 @@ while True :
 	price = input('請輸入商品價格')
 	price = int(price)
 	date = input('xxxx年,xx月,xx日')
-
-	#p =[]
-	#p.append(name)
-	#p.append(price)
-	#p = [name , price]
+	date =int(date)
 	products.append([name,price,date])
+print(products)
 
+#列出清單
 for p in products: #列出清單中所有資訊
 	print(p[0],'的價格是',p[1],'日期',p[2])
 
-
+#寫入CSV檔
 with open ('products.csv' , 'w', encoding = 'utf-8') as f :      #打開檔案
 	f.write('商品,價格,日期\n')
 	for p in products:                       #for 迴圈
